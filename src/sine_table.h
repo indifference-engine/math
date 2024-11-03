1e8752e557ab4d2092498ac09c3056f1bd7209bf
siliconspecter <siliconspecter@outlook.com>
2024-11-03 12:48:28 +0000

    Add sine table.

---
+++ src/sine_table.h
@@ -0,0 +1,11 @@
+#ifndef SINE_TABLE_H
+
+#define SINE_TABLE_H
+
+/**
+ * A table containing the first quarter of a sine curve.  Both 0 and 1 are
+ * included.
+ */
+extern const float sine_table[256];
+
+#endif
