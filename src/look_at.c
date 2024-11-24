45fec58422749623415857db5c1157f3fc6eb930
siliconspecter <siliconspecter@outlook.com>
2024-11-24 18:22:43 +0000

    Adjust to use Y+.

--- src/look_at.c
+++ src/look_at.c
@@ -8,9 +8,9 @@ void look_at(
     const float to_x,
     const float to_y,
     const float to_z,
-    const float z_x,
-    const float z_y,
-    const float z_z,
+    const float y_x,
+    const float y_y,
+    const float y_z,
     float *const forward,
     float *const inverse)
 {
@@ -24,37 +24,37 @@ void look_at(
   x_y *= x_inverse_magnitude;
   x_z *= x_inverse_magnitude;
 
-  float y_x = z_y * x_z - x_y * z_z;
-  float y_y = z_z * x_x - x_z * z_x;
-  float y_z = z_x * x_y - x_x * z_y;
+  float z_x = x_y * y_z - y_y * x_z;
+  float z_y = x_z * y_x - y_z * x_x;
+  float z_z = x_x * y_y - y_x * x_y;
 
-  const float y_inverse_magnitude = inverse_square_root(y_x * y_x + y_y * y_y + y_z * y_z);
+  const float z_inverse_magnitude = inverse_square_root(z_x * z_x + z_y * z_y + z_z * z_z);
 
-  y_x *= y_inverse_magnitude;
-  y_y *= y_inverse_magnitude;
-  y_z *= y_inverse_magnitude;
+  z_x *= z_inverse_magnitude;
+  z_y *= z_inverse_magnitude;
+  z_z *= z_inverse_magnitude;
 
-  float recalculated_z_x = x_y * y_z - y_y * x_z;
-  float recalculated_z_y = x_z * y_x - y_z * x_x;
-  float recalculated_z_z = x_x * y_y - y_x * x_y;
+  float recalculated_y_x = z_y * x_z - x_y * z_z;
+  float recalculated_y_y = z_z * x_x - x_z * z_x;
+  float recalculated_y_z = z_x * x_y - x_x * z_y;
 
-  const float recalculated_z_inverse_magnitude = inverse_square_root(recalculated_z_x * recalculated_z_x + recalculated_z_y * recalculated_z_y + recalculated_z_z * recalculated_z_z);
+  const float recalculated_y_inverse_magnitude = inverse_square_root(recalculated_y_x * recalculated_y_x + recalculated_y_y * recalculated_y_y + recalculated_y_z * recalculated_y_z);
 
-  recalculated_z_x *= recalculated_z_inverse_magnitude;
-  recalculated_z_y *= recalculated_z_inverse_magnitude;
-  recalculated_z_z *= recalculated_z_inverse_magnitude;
+  recalculated_y_x *= recalculated_y_inverse_magnitude;
+  recalculated_y_y *= recalculated_y_inverse_magnitude;
+  recalculated_y_z *= recalculated_y_inverse_magnitude;
 
   forward[0] = x_x;
-  forward[1] = y_x;
-  forward[2] = recalculated_z_x;
+  forward[1] = recalculated_y_x;
+  forward[2] = z_x;
   forward[3] = from_x;
   forward[4] = x_y;
-  forward[5] = y_y;
-  forward[6] = recalculated_z_y;
+  forward[5] = recalculated_y_y;
+  forward[6] = z_y;
   forward[7] = from_y;
   forward[8] = x_z;
-  forward[9] = y_z;
-  forward[10] = recalculated_z_z;
+  forward[9] = recalculated_y_z;
+  forward[10] = z_z;
   forward[11] = from_z;
   forward[12] = 0.0f;
   forward[13] = 0.0f;
@@ -65,14 +65,14 @@ void look_at(
   inverse[1] = x_y;
   inverse[2] = x_z;
   inverse[3] = -(from_x * x_x + from_y * x_y + from_z * x_z);
-  inverse[4] = y_x;
-  inverse[5] = y_y;
-  inverse[6] = y_z;
-  inverse[7] = -(from_x * y_x + from_y * y_y + from_z * y_z);
-  inverse[8] = recalculated_z_x;
-  inverse[9] = recalculated_z_y;
-  inverse[10] = recalculated_z_z;
-  inverse[11] = -(from_x * recalculated_z_x + from_y * recalculated_z_y + from_z * recalculated_z_z);
+  inverse[4] = recalculated_y_x;
+  inverse[5] = recalculated_y_y;
+  inverse[6] = recalculated_y_z;
+  inverse[7] = -(from_x * recalculated_y_x + from_y * recalculated_y_y + from_z * recalculated_y_z);
+  inverse[8] = z_x;
+  inverse[9] = z_y;
+  inverse[10] = z_z;
+  inverse[11] = -(from_x * z_x + from_y * z_y + from_z * z_z);
   inverse[12] = 0.0f;
   inverse[13] = 0.0f;
   inverse[14] = 0.0f;
--- src/look_at.h
+++ src/look_at.h
@@ -20,13 +20,13 @@
  * @param to_z The Z axis of the location to "look" at, in world space.
  *             Behavior is undefined if NaN, infinity or negative infinity, or
  *             coincident with the location to "look" from.
- * @param z_x The X axis of the normal along which to align the Z+ axis.
+ * @param y_x The X axis of the normal along which to align the Y+ axis.
  *            Behavior is undefined if NaN, aligned with from -> to or not unit
  *            length.
- * @param z_y The Y axis of the normal along which to align the Z+ axis.
+ * @param y_y The Y axis of the normal along which to align the Y+ axis.
  *            Behavior is undefined if NaN, aligned with from -> to or not unit
  *            length.
- * @param z_z The Z axis of the normal along which to align the Z+ axis.
+ * @param y_z The Z axis of the normal along which to align the Y+ axis.
  *            Behavior is undefined if NaN, aligned with from -> to or not unit
  *            length.
  * @param forward The 4x4 matrix to which to write the result.
@@ -39,9 +39,9 @@ void look_at(
     const float to_x,
     const float to_y,
     const float to_z,
-    const float z_x,
-    const float z_y,
-    const float z_z,
+    const float y_x,
+    const float y_y,
+    const float y_z,
     float *const forward,
     float *const inverse);
 
