6ab7b52037f3ec87bd3e3eb958b5d0d1f26a4e98
siliconspecter <siliconspecter@outlook.com>
2025-07-11 23:21:00 +0100

    Fix typo.

--- src/subtract_3_3.h
+++ src/subtract_3_3.h
@@ -8,7 +8,7 @@
  * component is NaN, infinity or negative infinity.
  * @param subtrahend The 3D vector to subtract by.  Behavior is undefined if any
  * component is NaN, infinity or negative infinity.
- * @param output The 3D vector to write the resulting subtracted 3D vector to.
+ * @param output The 3D vector to write the resulting difference 3D vector to.
  * May overlap with the input vectors.
  */
 void subtract_3_3(const float *const minuend, const float *const subtrahend,
